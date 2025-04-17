c9e87188204b41eb327bb1a1962c9f836ab63ec2
ReetamBG <r33t4m@gmail.com>
2025-04-17 10:50:57 +0530

    Some changes to package.json

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,7 @@ app.use(express.json({ limit: "1mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.MODE === "production" ? true : "http://localhost:5173",
+    origin: process.env.NODE_ENV === "production" ? true : "http://localhost:5173",
     credentials: true, // allow cookies
   })
 );
